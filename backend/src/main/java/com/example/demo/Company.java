467640a36a332dc6ee9b599feca265df11dc76ac
florinzamfir <florinzamfir@MacBook-Pro-florinzamfir.local>
2025-12-14 23:07:42 +0200

    test commit

--- backend/src/main/java/com/example/demo/Company.java
+++ backend/src/main/java/com/example/demo/Company.java
@@ -14,6 +14,7 @@ public class Company {
     private String uid;
 
     // Getters and setters
+    //add comment
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getName() { return name; }
